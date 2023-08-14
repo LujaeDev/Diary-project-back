6c2fdaeb6fa0cb45408e2340da670c73b738a5ac
youjae <youjae0803@gmail.com>
2023-08-14 14:46:59 +0900

    remove: delete files to manually register datasource

--- src/main/java/com/example/pproject/connection/ConnectionConst.java
+++
@@ -1,7 +0,0 @@
-package com.example.pproject.connection;
-
-public abstract class ConnectionConst {
-    public static final String URL = "jdbc:mysql://localhost/springPdb?characterEncoding=utf8";
-    public static final String USERNAME = "springP";
-    public static final String PASSWORD = "springP";
-}
