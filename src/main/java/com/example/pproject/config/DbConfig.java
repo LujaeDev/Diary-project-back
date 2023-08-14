19483d97057b571b62b0855a1214220bb96273b3
youjae <youjae0803@gmail.com>
2023-08-14 14:41:37 +0900

    refactor: auto config DataSource

--- src/main/java/com/example/pproject/config/DbConfig.java
+++ src/main/java/com/example/pproject/config/DbConfig.java
@@ -8,23 +8,23 @@ import javax.sql.DataSource;
 
 import static com.example.pproject.connection.ConnectionConst.*;
 
-@Configuration
-public class DbConfig {
-    @Bean
-    public DataSource dataSource(){
-        HikariDataSource ds = new HikariDataSource();
-
-        ds.setJdbcUrl(URL);
-        ds.setUsername(USERNAME);
-        ds.setPassword(PASSWORD);
-        ds.setMaximumPoolSize(10);
-
-        return ds;
-    }
+//@Configuration
+//public class DbConfig {
+//    @Bean
+//    public DataSource dataSource(){
+//        HikariDataSource ds = new HikariDataSource();
+//
+//        ds.setJdbcUrl(URL);
+//        ds.setUsername(USERNAME);
+//        ds.setPassword(PASSWORD);
+//        ds.setMaximumPoolSize(10);
+//
+//        return ds;
+//    }
 
     //MemberRepositroy가 사실상 memberDao의 역할을 하는거 아닌가?
 //    @Bean
 //    public MemberDao memberDao(){
 //        return new MemberDao(dataSource());
 //    }
-}
+//}
